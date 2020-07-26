ffb41682caebed5b3871fedad39de951925a0c21
Lukas E <lukas.eberle95@gmail.com>
2020-07-26 14:49:28 +0200

    Added basic data

--- python/rock-paper-scissors.py
+++ python/rock-paper-scissors.py
@@ -1,2 +1,10 @@
 import random
 
+print("-------------------")
+print("Rock Paper Scissors")
+print("-------------------")
+
+player_1 = input("Enter Player 1's name: ")
+player_2 = input("Enter Player 2's name: ")
+
+rolls = ['rock', 'paper', 'scissors']
