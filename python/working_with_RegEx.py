ae1ec93b58ad6d163d59c5062538910a83d5d312
Lukas E <lukas.eberle95@gmail.com>
2020-08-03 16:22:13 +0200

    Now extracting takes the Clipboard as input

--- python/working_with_RegEx.py
+++ python/working_with_RegEx.py
@@ -1,21 +1,32 @@
 import re
+import pyperclip
 
 
 def main():
-    fn = '../data/text.txt'
-    with open(fn, 'r', encoding='utf-8') as data:
-        msg = data.read()
-        print(get_contact_information(msg))
+    text = pyperclip.paste()
+    print(get_contact_information(text))
 
 
 def get_american_numbers(msg):
-    american_format = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+    """
+    We want to export American-Style phone numbers. Those could look like this:
+    415-555-0000, 555-0000, (415) 555-0000, 555-0000 ext 12345, ext. 12345, x12345
+    :param msg: Message is the text which should be searched
+    :return: A list of phone numbers
+    """
+    american_format = re.compile(r'(((\d\d\d)|(\(\d\d\d\)))?(\s|-)\d\d\d-\d\d\d\d((ext(\.)?\s|x)(\d{2,5}))?)')
     match_object = american_format.findall(msg)
     return match_object
 
 
 def get_german_numbers(msg):
-    german_format = re.compile(r'(\d\d\d\d|(\+|00)49\d\d\d)( \d\d\d\d\d\d)')
+    """
+    We want to export German-Style mobile phone numbers. Those could look like this:
+    0123 123456, +49123 123456, 0049123 123456
+    :param msg: Message is the text which should be searched
+    :return: A list of phone numbers
+    """
+    german_format = re.compile(r'((\d\d\d\d|(\+|00)49\d\d\d)( \d+))')
     match_object = german_format.findall(msg)
     return match_object
 
@@ -26,8 +37,17 @@ def get_email(msg):
     return match_object
 
 
+def get_main_group(numbers):
+    result = []
+    for i in range(0, len(numbers)):
+        result.append(numbers[i][0])
+    return result
+
+
 def get_contact_information(msg):
-    result = get_american_numbers(msg) + get_german_numbers(msg) + get_email(msg)
+    result = get_main_group(get_american_numbers(msg)) + get_main_group(get_german_numbers(msg)) + get_email(msg)
+    if not result:
+        return "There are no numbers or emails that could be extracted!"
     return result
 
 
