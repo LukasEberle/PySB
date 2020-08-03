c74b88c7a3bd526d0b195a248ab149e24a124bee
Lukas E <lukas.eberle95@gmail.com>
2020-08-03 13:38:56 +0200

    german number matching and cleanup of redundant code

--- python/working_with_RegEx.py
+++ python/working_with_RegEx.py
@@ -6,18 +6,19 @@ def main():
     with open(fn, 'r', encoding='utf-8') as data:
         msg = data.read()
         print(get_american_numbers(msg))
+        print(get_german_numbers(msg))
 
 
 def get_american_numbers(msg):
-    american_format = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+    american_format = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
     match_object = american_format.findall(msg)
-    if not match_object:
-        return None
     return match_object
 
 
 def get_german_numbers(msg):
-    german_format = re.compile(r'')
+    german_format = re.compile(r'(\d\d\d\d|(\+|00)49\d\d\d)( \d\d\d\d\d\d)')
+    match_object = german_format.findall(msg)
+    return match_object
 
 
 if __name__ == "__main__":
