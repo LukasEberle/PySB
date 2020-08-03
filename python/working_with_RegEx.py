04db91d8b939bd28892daeab3051962f96f28952
Lukas E <lukas.eberle95@gmail.com>
2020-08-03 14:35:42 +0200

    Added get_contact_information() and further test data

--- python/working_with_RegEx.py
+++ python/working_with_RegEx.py
@@ -5,8 +5,7 @@ def main():
     fn = '../data/text.txt'
     with open(fn, 'r', encoding='utf-8') as data:
         msg = data.read()
-        print(get_american_numbers(msg))
-        print(get_german_numbers(msg))
+        print(get_contact_information(msg))
 
 
 def get_american_numbers(msg):
@@ -20,10 +19,16 @@ def get_german_numbers(msg):
     match_object = german_format.findall(msg)
     return match_object
 
+
 def get_email(msg):
-    email_format = re.compile(r'\w+@\w+\.\w+')
+    email_format = re.compile(r'[a-zA-Z0-9.\-_]+@\w+\.\w+')
     match_object = email_format.findall(msg)
     return match_object
+
+
+def get_contact_information(msg):
+    result = get_american_numbers(msg) + get_german_numbers(msg) + get_email(msg)
+    return result
 
 
 if __name__ == "__main__":
