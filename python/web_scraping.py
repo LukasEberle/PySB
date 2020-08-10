21ec52d92a4eab2e936c82dbdd41b6a60d62e135
Lukas E <lukas.eberle95@gmail.com>
2020-08-10 10:29:17 +0200

    Further sandboxing

--- python/web_scraping.py
+++ python/web_scraping.py
@@ -1,7 +1,13 @@
-import webbrowser
+#import webbrowser
 import sys
 import requests
+import bs4
 
 
-webbrowser.open('https://antenneakiba.podigee.io/')
+#webbrowser.open('https://antenneakiba.podigee.io/about')
 sys.argv
+res = requests.get('https://antenneakiba.podigee.io/about')
+res.raise_for_status()
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
+elems = soup.select('#blog > div.section-main > div')
+print(elems[0].text.strip())
