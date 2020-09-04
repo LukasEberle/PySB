0d2b500ce672b0effe750dd36cfa3c041b572686
Lukas E <lukas.eberle95@gmail.com>
2020-09-04 18:00:29 +0200

    Further implementations added

--- python/time_estimation_improvement.py
+++ python/time_estimation_improvement.py
@@ -1,10 +1,14 @@
 import csv
+import json
 import sys
 from datetime import timedelta
 
-
+time_file = '../data/times_test.csv'
+goal_file = '../data/working_set.json'
 times = {}
-filename = '../data/times_test.csv'
+with open(goal_file, 'r', encoding='utf-8') as data:
+    times = json.load(data)
+
 #arguments = sys.argv[1:]
 #if len(arguments) < 1:
 #    print("Please enter a filename")
@@ -15,6 +19,11 @@ filename = '../data/times_test.csv'
 def main():
     csv.Dialect.delimiter = ','
     load_times()
+    text_time = "00:15:26"
+    print(str_h(text_time))
+    print(str_min(text_time))
+    print(str_sec(text_time))
+    print(str_sec(text_time)+str_min(text_time))
 
 
 def new_prediction(used_time, predicted_time, alpha=0.1):
@@ -22,13 +31,25 @@ def new_prediction(used_time, predicted_time, alpha=0.1):
 
 
 def load_times():
-    with open(filename, 'r', encoding='utf-8') as data:
+    with open(time_file, 'r', encoding='utf-8') as data:
         time_reader = csv.reader(data, delimiter=',')
         next(time_reader)
         current_line = (next(time_reader))
         project = current_line[3]
         duration = current_line[11]
         print(f"You worked {duration} hours on {project}!")
+
+
+def str_h(time_text):
+    return int(time_text[0:2])
+
+
+def str_min(time_text):
+    return int(time_text[3:5])
+
+
+def str_sec(time_text):
+    return int(time_text[6:])
 
 
 if __name__ == '__main__':
