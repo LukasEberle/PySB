2b5ff2ce920dede94583084117e43075011b2fd5
Lukas E <lukas.eberle95@gmail.com>
2020-09-04 17:10:34 +0200

    Now we can read times and projects from the .csv-file

--- python/time_estimation_improvement.py
+++ python/time_estimation_improvement.py
@@ -1,11 +1,20 @@
 import csv
+import sys
 from datetime import timedelta
 
+
 times = {}
+filename = '../data/times_test.csv'
+#arguments = sys.argv[1:]
+#if len(arguments) < 1:
+#    print("Please enter a filename")
+#else:
+#    filename = arguments[0]
 
 
 def main():
-    pass
+    csv.Dialect.delimiter = ','
+    load_times()
 
 
 def new_prediction(used_time, predicted_time, alpha=0.1):
@@ -14,7 +23,12 @@ def new_prediction(used_time, predicted_time, alpha=0.1):
 
 def load_times():
     with open(filename, 'r', encoding='utf-8') as data:
-        pass
+        time_reader = csv.reader(data, delimiter=',')
+        next(time_reader)
+        current_line = (next(time_reader))
+        project = current_line[3]
+        duration = current_line[11]
+        print(f"You worked {duration} hours on {project}!")
 
 
 if __name__ == '__main__':
