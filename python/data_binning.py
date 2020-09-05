07fbf8a54a7ae916411ba7b1679d97c113db1d8c
Lukas E <lukas.eberle95@gmail.com>
2020-09-05 10:22:30 +0200

    Fixed errors

--- python/data_binning.py
+++ python/data_binning.py
@@ -1,28 +1,31 @@
+import math
+
+
 input1 = [0, 4, 12, 16, 16, 18, 24, 26, 28]
 input2 = [2, 6, 7, 9, 13, 20, 21, 25, 30]
 input3 = [5, 10, 11, 13, 15, 35, 50, 55, 72, 92, 204, 215]
 
 
 def main():
-    print(f"Equal width of input1 is {equal_width(input1, 3)}\n")
+    print(f"Equal width of input1 is {equal_width(input1, 3)}")
     print(f"Equal frequency of input1 is {equal_frequency(input1, 3)}\n")
-    print(f"Equal width of input2 is {equal_width(input2, 3)}\n")
+    print(f"Equal width of input2 is {equal_width(input2, 3)}")
     print(f"Equal frequency of input2 is {equal_frequency(input2, 3)}\n")
-    print(f"Equal width of input3 is {equal_width(input3, 3)}\n")
+    print(f"Equal width of input3 is {equal_width(input3, 3)}")
     print(f"Equal frequency of input3 is {equal_frequency(input3, 3)}\n")
 
 
 def equal_width(arr, k):
     min_arr = min(arr)
-    w = int((max(arr)-min_arr)/k)   #Width of the interval
+    w = int(math.ceil((max(arr)-min_arr)/k))   #Width of the interval
     result = []
     boundaries = []
     for i in range(0, k+1):
         boundaries = boundaries + [min_arr + w * i]
-    for i in range(0,k):
+    for i in range(0, k):
         tmp = []
         for j in arr:
-            if arr[i] < j < arr[i + 1]:
+            if boundaries[i] <= j < boundaries[i + 1]:
                 tmp += [j]
         result += [tmp]
     return result
@@ -39,6 +42,7 @@ def equal_frequency(arr, k):
                 break
             tmp = tmp + [arr[j]]
         result.append(tmp)
+    return result
 
 
 if __name__ == '__main__':
