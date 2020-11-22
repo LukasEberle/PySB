0a05f032922f8c68bfb6e796d375763bec02a84e
Lukas E <lukas.eberle95@gmail.com>
2020-11-22 11:23:38 +0100

    refined structure with some actual code

--- python/book-to-pdf.py
+++ python/book-to-pdf.py
@@ -1,8 +1,28 @@
 import PyPDF2
 
-# pdfFile = open(path, 'rb') # rb = read Binary
-# reader = PyPDF2.PdfFileReader(pdfFile)
-# totalPages = reader.numPages
-# tmp_writer = PyPDF2.PdfFileWriter()
 # loop over PDF File add all pages expect the last, add blank pages until the total pages mod 4 = 0
 # reorder recursively
+
+
+def add_blanks(reader, writer):
+    pass
+
+
+def reformat(page, reader, writer):
+    pass
+
+
+path_to_file = ""
+pdfFile = open(path_to_file, 'rb') # rb = read Binary
+org_reader = PyPDF2.PdfFileReader(pdfFile)
+totalPages = reader.numPages
+if not ((totalPages % 4) == 0):
+    tmp_writer = PyPDF2.PdfFileWriter()
+    add_blanks(tmp_writer, org_reader)
+    # dump writer and open as reader and pass it instead of org_reader
+    tmp_writer.close()
+    tmp_writer = PyPDF2.PdfFileWriter()
+    reformat(1, org_reader, tmp_writer)
+else:
+    tmp_writer = PyPDF2.PdfFileWriter()
+    reformat(1, org_reader, tmp_writer)
