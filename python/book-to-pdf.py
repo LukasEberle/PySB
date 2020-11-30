5ea36c66730895d8d1352178b252e4cfab1a0478
Lukas E <lukas.eberle95@gmail.com>
2020-11-30 18:31:01 +0100

    Completed and tested with mod4 book (Book back right)

--- python/book-to-pdf.py
+++ python/book-to-pdf.py
@@ -6,10 +6,8 @@ def add_blanks(pages, reader, writer):
     assert to_add != 0
     for i in range(0, (pages-2)):
         writer.addPage(reader.getPage(i))
-    blank_file = open(path_to_file, 'rb')
-    blank_reader = PyPDF2.PdfFileReader(blank_file)
     while to_add > 0:
-        writer.addPage(blank_reader.getPage(0))
+        writer.addBlankPage()
         to_add -= 1
     writer.addPage(reader.getPage(pages-1))
 
@@ -18,13 +16,13 @@ def reformat(pages, reader, writer):
     for i in range(0, pages, 2):
         if i < pages-i:
             writer.addPage(reader.getPage(i))
-            writer.addPage(reader.getPage(pages-i))
             writer.addPage(reader.getPage(pages-(i+1)))
+            writer.addPage(reader.getPage(pages-(i+2)))
             writer.addPage(reader.getPage(i + 1))
 
 
-path_to_file = ""
-path_to_blank = ""
+path_to_file = "../data/mausritter-rules.pdf"
+output_file = "../data/mausritter-rules_printable.pdf"
 pdfFile = open(path_to_file, 'rb') # rb = read Binary
 org_reader = PyPDF2.PdfFileReader(pdfFile)
 totalPages = org_reader.numPages
@@ -34,7 +32,10 @@ if not ((totalPages % 4) == 0):
     # dump writer and open as reader and pass it instead of org_reader
     tmp_writer.close()
     tmp_writer = PyPDF2.PdfFileWriter()
-    reformat(1, org_reader, tmp_writer)
+    reformat(totalPages, org_reader, tmp_writer)
 else:
     tmp_writer = PyPDF2.PdfFileWriter()
-    reformat(1, org_reader, tmp_writer)
+    reformat(totalPages, org_reader, tmp_writer)
+wb_output = open(output_file, 'wb')
+tmp_writer.write(wb_output)
+wb_output.close()
