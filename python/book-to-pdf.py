a8bece6fc8b60d67d992d9d94a5686e5060a6648
Lukas E <lukas.eberle95@gmail.com>
2020-11-30 18:38:23 +0100

    Added parameter to change the bookback to left or right

--- python/book-to-pdf.py
+++ python/book-to-pdf.py
@@ -12,17 +12,25 @@ def add_blanks(pages, reader, writer):
     writer.addPage(reader.getPage(pages-1))
 
 
-def reformat(pages, reader, writer):
+def reformat(pages, reader, writer, western):
     for i in range(0, pages, 2):
-        if i < pages-i:
-            writer.addPage(reader.getPage(i))
-            writer.addPage(reader.getPage(pages-(i+1)))
-            writer.addPage(reader.getPage(pages-(i+2)))
-            writer.addPage(reader.getPage(i + 1))
+        if western:
+            if i < pages - i:
+                writer.addPage(reader.getPage(pages - (i + 1)))
+                writer.addPage(reader.getPage(i))
+                writer.addPage(reader.getPage(i + 1))
+                writer.addPage(reader.getPage(pages - (i + 2)))
+        if not western:
+            if i < pages-i:
+                writer.addPage(reader.getPage(i))
+                writer.addPage(reader.getPage(pages - (i + 1)))
+                writer.addPage(reader.getPage(pages - (i + 2)))
+                writer.addPage(reader.getPage(i + 1))
 
 
 path_to_file = "../data/mausritter-rules.pdf"
 output_file = "../data/mausritter-rules_printable.pdf"
+book_back_left = True
 pdfFile = open(path_to_file, 'rb') # rb = read Binary
 org_reader = PyPDF2.PdfFileReader(pdfFile)
 totalPages = org_reader.numPages
@@ -32,10 +40,10 @@ if not ((totalPages % 4) == 0):
     # dump writer and open as reader and pass it instead of org_reader
     tmp_writer.close()
     tmp_writer = PyPDF2.PdfFileWriter()
-    reformat(totalPages, org_reader, tmp_writer)
+    reformat(totalPages, org_reader, tmp_writer, book_back_left)
 else:
     tmp_writer = PyPDF2.PdfFileWriter()
-    reformat(totalPages, org_reader, tmp_writer)
+    reformat(totalPages, org_reader, tmp_writer, book_back_left)
 wb_output = open(output_file, 'wb')
 tmp_writer.write(wb_output)
 wb_output.close()
