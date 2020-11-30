c05a0ce7a86c39cd67852787556749e774c5e761
Lukas E <lukas.eberle95@gmail.com>
2020-11-30 18:51:47 +0100

    Non-mod4 book support added (not tested yet)

--- python/book-to-pdf.py
+++ python/book-to-pdf.py
@@ -1,15 +1,5 @@
 import PyPDF2
-
-
-def add_blanks(pages, reader, writer):
-    to_add = pages % 4
-    assert to_add != 0
-    for i in range(0, (pages-2)):
-        writer.addPage(reader.getPage(i))
-    while to_add > 0:
-        writer.addBlankPage()
-        to_add -= 1
-    writer.addPage(reader.getPage(pages-1))
+import os
 
 
 def reformat(pages, reader, writer, western):
@@ -36,14 +26,19 @@ org_reader = PyPDF2.PdfFileReader(pdfFile)
 totalPages = org_reader.numPages
 if not ((totalPages % 4) == 0):
     tmp_writer = PyPDF2.PdfFileWriter()
-    add_blanks(tmp_writer, org_reader)
-    # dump writer and open as reader and pass it instead of org_reader
-    tmp_writer.close()
-    tmp_writer = PyPDF2.PdfFileWriter()
-    reformat(totalPages, org_reader, tmp_writer, book_back_left)
+    tmp_writer.insertBlankPage(index=(totalPages - 2))
+    added_blanks = open('../data/tmp.pdf', 'wb')
+    tmp_writer.write(added_blanks)
+    added_blanks.close()
+    pdfFile.close()
+    prepared_reader = open('../data/tmp.pdf', 'rb')
+    book_writer = PyPDF2.PdfFileWriter()
+    reformat(totalPages, prepared_reader, book_writer, book_back_left)
+    prepared_reader.close()
 else:
-    tmp_writer = PyPDF2.PdfFileWriter()
-    reformat(totalPages, org_reader, tmp_writer, book_back_left)
+    book_writer = PyPDF2.PdfFileWriter()
+    reformat(totalPages, org_reader, book_writer, book_back_left)
+    pdfFile.close()
 wb_output = open(output_file, 'wb')
 tmp_writer.write(wb_output)
 wb_output.close()
