9030f122c84b39b4eeda255d8e12056bfc17b4f8
Lukas E <lukas.eberle95@gmail.com>
2021-04-26 14:40:36 +0200

    Light restructuring

--- python/importable_vocabulary.py
+++ python/importable_vocabulary.py
@@ -1,4 +1,3 @@
-import csv
 import sys
 from os import listdir
 from os.path import isfile, join
@@ -30,8 +29,7 @@ def read_vocabulary(rows, table):
     tags = generate_tags(table)
     with open(join(vocabulary_dir, table), "r", encoding="utf-8", errors="ignore") as data:
         for entry in data.readlines():
-            cleaned_data = entry[:-1]
-            cleaned_data = cleaned_data.split(';')
+            cleaned_data = clean_data(entry)
             cleaned_data.append(tags)
             rows.append(cleaned_data)
 
@@ -42,14 +40,33 @@ def generate_tags(raw):
 
     :param raw:  A string containing the tags. It has the form "tag.csv".
                  Multiple tags are concatenated with '+' ("tag1+tag2.csv").
+    :return: A list of tags
     """
     tag_string = raw[:-4]
     return tag_string.split("+")
 
 
 def embolden_kanji(rows):
+    """
+    Adds HTML tags to embolden the Kanji Characters.
+
+    :param rows: A list of lists with the Kanji Character in the first spot. It will be updated.
+    """
     for r in rows:
-        r[0] = "<b>" + r[0] + "</b>"
+        if not r[0] == " ":
+            r[0] = "<b>" + r[0] + "</b>"
+
+
+def clean_data(data):
+    """
+    Takes in the current data row, cleans it and returns a list of all entries.
+
+    :param data: Current noisy data
+    :return: List of cell content in the row
+    """
+    result = data[:-1]
+    result = result.split(';')
+    return result
 
 
 if __name__ == "__main__":
